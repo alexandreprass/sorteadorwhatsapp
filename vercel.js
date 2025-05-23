b204f367d54e31b12a129dc204489c991fb72a4c
alexandreprass <162365360+alexandreprass@users.noreply.github.com>
2025-05-23 17:53:29 -0300

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -2,14 +2,19 @@
   "version": 2,
   "builds": [
     {
-      "src": "index.js",
+      "src": "api/index.js",
       "use": "@vercel/node"
     }
   ],
   "routes": [
     {
       "src": "/(.*)",
-      "dest": "/index.js"
+      "dest": "/api/index.js"
     }
-  ]
+  ],
+  "functions": {
+    "api/index.js": {
+      "maxDuration": 45 // Aumenta o tempo limite para 45 segundos (ou até 60 no Hobby)
+    }
+  }
 }
