9e9bfa8eb387a6a4365936acd7f0e8e36a907b27
alexandreprass <162365360+alexandreprass@users.noreply.github.com>
2025-05-23 16:32:21 -0300

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,16 +1,15 @@
-// vercel.json (se o index.js estiver NA RAIZ do projeto)
 {
   "version": 2,
   "builds": [
     {
-      "src": "index.js",  // Alterado para o arquivo na raiz
+      "src": "index.js",
       "use": "@vercel/node"
     }
   ],
   "routes": [
     {
       "src": "/(.*)",
-      "dest": "/index.js" // Alterado para o arquivo na raiz
+      "dest": "/index.js"
     }
   ]
 }
